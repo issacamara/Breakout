31bd6bfe81bb16ec5ea95fd0ea0c7a2ac4faad8f
Issa CAMARA <isskamara@live.fr>
2015-05-25 00:39:51 +0200

    Adding the stopper

--- Breakout/BreakoutViewController.swift
+++ Breakout/BreakoutViewController.swift
@@ -12,24 +12,26 @@ class BreakoutViewController: UIViewController {
     
     @IBOutlet weak var gameView: UIView!
     
-    let bricksPerRow = 50
-    let rowsPerGame = 10
-    let offset = 100
+    
+    let bricksPerRow = 25
+    let rowsPerGame = 5
+    let topOffset = 70
+    let widthOffset = 10
     
     var brickSize: CGSize{
-        let size = (gameView.bounds.size.width - 2 * CGFloat(offset)) / CGFloat(bricksPerRow)
-        return CGSize(width: size, height: size)
+        let size = (gameView.bounds.size.width - 2 * CGFloat(widthOffset)) / CGFloat(bricksPerRow)
+        return CGSize(width: size, height: size / 2)
     }
 
     
     func drawBricks(){
         
-        for i in 0...rowsPerGame {
+        for i in 0...rowsPerGame - 1 {
             var frame = CGRect(origin: CGPointZero, size: brickSize)
-            frame.origin.y = CGFloat(i) * brickSize.width
-            for j in 0...bricksPerRow {
+            frame.origin.y = CGFloat(i) * brickSize.height + CGFloat(topOffset)
+            for j in 0...bricksPerRow - 1 {
                 
-                frame.origin.x = CGFloat(j) * brickSize.width + CGFloat(offset)
+                frame.origin.x = CGFloat(j) * brickSize.width + CGFloat(widthOffset)
                 
                 let brickView = Brick(frame: frame, coordinate: CGPoint(x: i, y: j))
                 brickView.backgroundColor = UIColor.random
@@ -41,7 +43,15 @@ class BreakoutViewController: UIViewController {
         
     }
     
+    @IBOutlet weak var stopper: Stopper!
     
+    @IBAction func moveStopper(sender: UIPanGestureRecognizer) {
+        let location = sender.locationInView(gameView)
+        if location.x >= CGFloat(widthOffset) && location.x <= (gameView.bounds.size.width - CGFloat(widthOffset) - stopper.frame.width) {
+            let newPosition = CGPoint(x: location.x, y: stopper.frame.origin.y)
+            stopper.frame = CGRect(origin: newPosition, size: stopper.frame.size)
+        }
+    }
     
     
     
--- Breakout/Stopper.swift
+++ Breakout/Stopper.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
-class Stopper: NSObject {
-   
+class Stopper: UIView {
+    
+       
 }
